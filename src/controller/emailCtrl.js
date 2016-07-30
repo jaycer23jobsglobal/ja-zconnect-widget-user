15b92528e02a1f3948728ab1b86f515b557911be
Jaycer William Abante <jaycer.a23@gmail.com>
2016-07-30 10:43:50 +0400

    add change password

--- src/controller/emailCtrl.js
+++ src/controller/emailCtrl.js
@@ -1,8 +1,10 @@
 angular.module('ja-zconnect-widget-user')
-    .controller('EmailCtrl', ['currentUser', '$q', 'ngZconnected', 'profileService', 'jobService', function(currentUser, $q, ngZconnected, profileService, jobService) {
+    .controller('EmailCtrl', ['currentUser', '$q', 'ngZconnected', 'profileService', 'userService', function(currentUser, $q, ngZconnected, profileService, userService) {
         var vm = this;
         vm.currentUser = currentUser;
         vm.email = {};
+        vm.password = {};
+        vm.not_valid = null;
 
         $q.when(profileService.userEmail.get(vm.currentUser.user_id, vm.currentUser.extra_properties.primary_email))
             .then(function(email) {
@@ -13,6 +15,49 @@ angular.module('ja-zconnect-widget-user')
                 if (ngZconnected._DEBUG)
                     console.log(error);
             });
+
+        vm.showInputPassword = function()
+        {
+            $("#inputPassword").show();
+            $("#password").hide();
+        }
+
+        vm.cancelEditPassword = function()
+        {
+            $("#inputPassword").hide();
+            $("#password").show();
+
+            vm.not_valid = null;
+            vm.password = {};
+        }
+
+        vm.editPassword = function(form)
+        {
+            if (form.$valid) {
+            if(vm.password.current_password.length >= 8){
+                    if(vm.password.new_password.length >= 8){
+                        if(vm.password.retype_password.length >= 8){
+                            if(vm.password.new_password == vm.password.retype_password){
+                                vm.not_valid = null;
+                                vm.password.id = vm.currentUser.user_id;
+                                userService.password.save(vm.currentUser.user_id, vm.password)
+                                .then(function(response) {
+                                    if (ngZconnected._DEBUG)
+                                        console.log(response);
+                                        Zconnected.helpers.showSystemMessage(response.message, "message");
+                                        vm.cancelEditPassword();
+                                }, function(error) {
+                                    if (ngZconnected._DEBUG)
+                                        console.log(error);
+                                });
+                            } else {
+                                vm.not_valid = "New password not match on Retype password.";
+                            }
+                    }
+                }
+            }
+        }
+        }
     }]);
 
 
